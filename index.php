38131a2e2dd58a904609b737b84a24099bc98075
oscarpon <oscar.pontebaquero@gmail.com>
2019-10-02 20:35:36 +0200

    Bootstrap

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<link rel="stylesheet" href=css/bootstrap.min.css>
   <head>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
 
-    <?php
-      print "Buenos dias a todos esto es una prueba de mensaje aaaaaa"
-
-     ?>
+    <form action="/action_page.php">
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
 
   </body>
 </html>
