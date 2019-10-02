a984f40c62d0b2fff2bb4246269031a9d1cb6c3f
oscarpon <oscar.pontebaquero@gmail.com>
2019-10-02 20:22:40 +0200

    Prueba

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      print "Buenos dias a todos esto es una prueba de mensaje aaaa"
+
+     ?>
+
+  </body>
+</html>
