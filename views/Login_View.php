5c6f245fdbc34757951f958263087e69538b011f
BraisRM <brmartinez@esei.uvigo.es>
2019-10-15 19:06:07 +0200

    Arreglado el error del merge fatal puto de mierda

--- views/Login_View.php
+++ views/Login_View.php
@@ -1,45 +1,34 @@
 <?php
-
 	class Login{
-
 /////////////////
 		function __construct(){
 			$this->render();
 		}
 ////////////////
 		function render(){
-			
-				include '../views/Header.php';
-
-
+			include '../views/Header.php';
 ?>
 
-	<div class="formularioLogin">
-	 <form id="login" action='../controllers/Login_Controller.php' method='post'>
-		 <h3 id="iniciar">Iniciar Sesión</h3>
-	<div>
-			<label id="email1" for="email"></label>
-			<input type="email" id="email1" class="form-control" aria-describedby="emailHelp" placeholder="&#x1F464; Introduce tu email" onblur="comprobarEmail(email, 50)">
-
-	</div>
-	<div>
-	 <label for="password"></label>
-	 <input type="password" id="password" placeholder="&#128273; Contraseña" >
-	</div>
-	<div>
-	 <a href="../controllers/Register_Controller.php" id="nocuenta">¿Aún no tienes cuenta?</a>
-	</div>
-	 <button onclick="validarLogin" type="submit" class="botonAcceder">Acceder</button>
-	</form>
+<div class="formularioLogin">
+	<form id="login" action='../controllers/Login_Controller.php' method='post'>
+		<h3 id="iniciar">Iniciar Sesión</h3>
+<div>
+		<label id="email1" for="email"></label>
+		<input type="email" id="email1" aria-describedby="emailHelp" placeholder="&#x1F464; Introduce tu email" onblur="comprobarEmail(email, 50)">
 </div>
-
-
-
+<div>
+	<label for="password"></label>
+	<input type="password" id="password" placeholder="&#128273; Contraseña" >
+</div>
+<div>
+	<a href="../controllers/Register_Controller.php" id="nocuenta">¿Aún no tienes cuenta?</a>
+</div>
+	<button onclick="validarLogin" type="submit" class="botonAcceder">Acceder</button>
+</form>
+</div>
 
 <?php
 			include '../Views/Footer.php';
 		} //fin metodo render
-
 	} //fin Login
-
 ?>
--- views/Register_View.php
+++ views/Register_View.php
@@ -51,6 +51,4 @@ class Register{
 include '../views/Footer.php';
   }
 }
-
-
  ?>
