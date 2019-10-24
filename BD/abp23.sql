99012bcf68a450c29c2e27fcd91f49889eeb2a6f
BraisRM <brmartinez@esei.uvigo.es>
2019-10-24 19:08:33 +0200

    Base de datos ok

--- BD/abp23.sql
+++ BD/abp23.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-10-2019 a las 17:53:42
+-- Tiempo de generación: 24-10-2019 a las 19:06:26
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.9
-
-DROP DATABASE IF EXISTS `abp23`;
-CREATE DATABASE `abp23` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,8 +29,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `asistenciasclases` (
-  `nombre` varchar(20) NOT NULL,
-  `horario` datetime(6) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `horario` datetime NOT NULL,
   `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -47,7 +44,7 @@ CREATE TABLE `campeonato` (
   `nombre` varchar(25) NOT NULL,
   `fechaFinIns` date NOT NULL,
   `categoria` char(1) NOT NULL,
-  `genero` char(6) NOT NULL
+  `genero` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,9 +54,9 @@ CREATE TABLE `campeonato` (
 --
 
 CREATE TABLE `clasesformativas` (
-  `nombre` varchar(20) NOT NULL,
-  `horario` datetime(6) NOT NULL,
-  `entrenador` varchar(20) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `horario` datetime NOT NULL,
+  `entrenador` varchar(50) NOT NULL,
   `codigoPista` char(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -70,8 +67,8 @@ CREATE TABLE `clasesformativas` (
 --
 
 CREATE TABLE `clasificacion` (
-  `miembro1` varchar(10) NOT NULL,
-  `miembro2` varchar(10) NOT NULL,
+  `miembro1` varchar(50) NOT NULL,
+  `miembro2` varchar(50) NOT NULL,
   `nombreCamp` varchar(25) NOT NULL,
   `grupo` char(1) NOT NULL,
   `puntos` int(2) NOT NULL
@@ -84,9 +81,9 @@ CREATE TABLE `clasificacion` (
 --
 
 CREATE TABLE `contenido` (
-  `idContenido` char(6) NOT NULL,
+  `idContenido` int(6) NOT NULL,
   `titulo` varchar(10) NOT NULL,
-  `descripcion` char(50) NOT NULL
+  `descripcion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -96,8 +93,8 @@ CREATE TABLE `contenido` (
 --
 
 CREATE TABLE `incripcionesclases` (
-  `nombre` varchar(20) NOT NULL,
-  `horario` datetime(6) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `horario` datetime NOT NULL,
   `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -123,8 +120,30 @@ CREATE TABLE `oferprompartidos` (
 --
 
 INSERT INTO `oferprompartidos` (`email`, `fecha`, `partic1`, `partic2`, `partic3`, `partic4`, `numpart`, `tipo`) VALUES
-('rachid1194@hotmail.com', '2019-10-20 18:46:54', 'rachid1194@hotmail.com', 'Puesto vacio', 'Puesto vacio', 'Puesto vacio', 4, 'OFER'),
-('rachid1194@hotmail.com', '2019-10-20 18:47:14', 'rachid1194@hotmail.com', 'Puesto vacio', 'Puesto vacio', 'Puesto vacio', 1, 'OFER');
+('brmartinez@esei.uvigo.es', '2019-10-22 19:13:21', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 4, 'PROM');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `email` varchar(50) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `importe` int(3) NOT NULL,
+  `pagado` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pagos`
+--
+
+INSERT INTO `pagos` (`email`, `fecha`, `importe`, `pagado`) VALUES
+('rachid1194@hotmail.com', '2019-10-22 19:00:55', 25, 'S'),
+('rachid1194@hotmail.com', '2019-10-22 19:03:50', 50, 'S'),
+('rachid1194@hotmail.com', '2019-10-22 19:04:15', 60, 'S'),
+('trelo@gmail.com', '2019-10-22 19:15:39', 25, 'S');
 
 -- --------------------------------------------------------
 
@@ -133,9 +152,10 @@ INSERT INTO `oferprompartidos` (`email`, `fecha`, `partic1`, `partic2`, `partic3
 --
 
 CREATE TABLE `parejas` (
-  `miembro1` varchar(10) NOT NULL,
-  `miembro2` varchar(10) NOT NULL,
-  `capitan` varchar(10) NOT NULL
+  `miembro1` varchar(50) NOT NULL,
+  `miembro2` varchar(50) NOT NULL,
+  `capitan` varchar(50) NOT NULL,
+  `genero` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -145,8 +165,8 @@ CREATE TABLE `parejas` (
 --
 
 CREATE TABLE `partcampeonatos` (
-  `miembro1` varchar(10) NOT NULL,
-  `miembro2` varchar(10) NOT NULL,
+  `miembro1` varchar(50) NOT NULL,
+  `miembro2` varchar(50) NOT NULL,
   `nombreCamp` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -158,11 +178,11 @@ CREATE TABLE `partcampeonatos` (
 
 CREATE TABLE `partidocamp` (
   `codigoPista` char(6) NOT NULL,
-  `fecha` datetime(6) NOT NULL,
-  `miembro1Par1` varchar(10) NOT NULL,
-  `miembro2Par1` varchar(10) NOT NULL,
-  `miembro1Par2` varchar(10) NOT NULL,
-  `miembro2Par2` varchar(10) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `miembro1Par1` varchar(50) NOT NULL,
+  `miembro2Par1` varchar(50) NOT NULL,
+  `miembro1Par2` varchar(50) NOT NULL,
+  `miembro2Par2` varchar(50) NOT NULL,
   `nombreCamp` varchar(25) NOT NULL,
   `resultado` char(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -175,7 +195,7 @@ CREATE TABLE `partidocamp` (
 
 CREATE TABLE `partidos` (
   `codigoPista` char(6) NOT NULL,
-  `fecha` datetime(6) NOT NULL,
+  `fecha` datetime NOT NULL,
   `miembro1Par1` varchar(50) NOT NULL,
   `miembro2Par1` varchar(50) NOT NULL,
   `miembro1Par2` varchar(50) NOT NULL,
@@ -188,7 +208,11 @@ CREATE TABLE `partidos` (
 --
 
 INSERT INTO `partidos` (`codigoPista`, `fecha`, `miembro1Par1`, `miembro2Par1`, `miembro1Par2`, `miembro2Par2`, `resultado`) VALUES
-('000000', '2019-10-18 13:59:02.000000', '', '', '', '', 'NJ');
+('000000', '2019-10-18 13:59:02', 'rachid1194@hotmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'NJ'),
+('000000', '2019-10-18 14:59:02', 'rachid1194@hotmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'NJ'),
+('000000', '2019-10-18 15:59:02', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'NJ'),
+('000001', '2019-10-18 13:59:02', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'NJ'),
+('000001', '2019-10-18 16:59:02', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'trelo@gmail.com', 'NJ');
 
 -- --------------------------------------------------------
 
@@ -198,8 +222,8 @@ INSERT INTO `partidos` (`codigoPista`, `fecha`, `miembro1Par1`, `miembro2Par1`, 
 
 CREATE TABLE `pistas` (
   `codigoPista` char(6) NOT NULL,
-  `fecha` datetime(6) NOT NULL,
-  `tipo` char(6) NOT NULL
+  `fecha` datetime NOT NULL,
+  `tipo` char(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -207,11 +231,11 @@ CREATE TABLE `pistas` (
 --
 
 INSERT INTO `pistas` (`codigoPista`, `fecha`, `tipo`) VALUES
-('000000', '2019-10-18 13:59:02.000000', 'EXT'),
-('000000', '2019-10-18 14:59:02.000000', 'EXT'),
-('000000', '2019-10-18 15:59:02.000000', 'EXT'),
-('000001', '2019-10-18 13:59:02.000000', 'EXT'),
-('000001', '2019-10-18 16:59:02.000000', 'EXT');
+('000000', '2019-10-18 13:59:02', 'EXT'),
+('000000', '2019-10-18 14:59:02', 'EXT'),
+('000000', '2019-10-18 15:59:02', 'EXT'),
+('000001', '2019-10-18 13:59:02', 'EXT'),
+('000001', '2019-10-18 16:59:02', 'EXT');
 
 -- --------------------------------------------------------
 
@@ -222,7 +246,7 @@ INSERT INTO `pistas` (`codigoPista`, `fecha`, `tipo`) VALUES
 CREATE TABLE `reservas` (
   `email` varchar(50) NOT NULL,
   `codigoPista` char(6) NOT NULL,
-  `fecha` datetime(6) NOT NULL
+  `fecha` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -237,7 +261,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(20) NOT NULL,
   `apellidos` varchar(30) NOT NULL,
   `rol` char(1) NOT NULL,
-  `genero` char(6) NOT NULL
+  `genero` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -245,8 +269,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`email`, `password`, `nombre`, `apellidos`, `rol`, `genero`) VALUES
-('brmartinez@esei.uvigo.es', 'hola', 'Brais', 'RM', 'A', 'Mascul'),
-('rachid1194@hotmail.com', 'm', 'h', 'r', 'D', 'M');
+('brmartinez@esei.uvigo.es', 'hola', 'Brais', 'RM', 'A', 'M'),
+('rachid1194@hotmail.com', 'm', 'h', 'r', 'D', 'M'),
+('trelo@gmail.com', 'trelo', 'Trelo', 'olo', 'D', 'O');
 
 --
 -- Índices para tablas volcadas
@@ -295,6 +320,12 @@ ALTER TABLE `oferprompartidos`
   ADD PRIMARY KEY (`email`,`fecha`);
 
 --
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`email`,`fecha`);
+
+--
 -- Indices de la tabla `parejas`
 --
 ALTER TABLE `parejas`
@@ -335,6 +366,16 @@ ALTER TABLE `reservas`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  MODIFY `idContenido` int(6) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
