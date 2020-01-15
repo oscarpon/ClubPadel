3f0e95c7d7075413d3a0da36b51a574b7a8f1b37
oscarpon <oscar.pontebaquero@gmail.com>
2020-01-15 19:07:45 +0100

    Escuela deportiva ADD a medias

--- models/EscuelaDeportiva_Model.php
+++ models/EscuelaDeportiva_Model.php
@@ -27,6 +27,45 @@ class EscuelaDeportivaModel{
     $resultado = $this->mysqli->query($sql);
     return $resultado;
   }
+  function añadirEscuela(){
+    if (($this->nombre <> '')){
+        $sql = "SELECT * FROM escuelasdeportivas WHERE (nombre = '$this->nombre')";
+		if (!$result = $this->mysqli->query($sql)){
+			return 'Imposible ConectarBD';
+		}
+		else {
+			if ($result->num_rows == 0){
+
+				$sql = "INSERT INTO escuelasdeportivas (
+          nombre,
+					fechaFinIns,
+					categoria,
+					genero,
+					estado
+					)
+						VALUES (
+            '$this->nombre',
+						'$this->fechaFinIns',
+						'$this->categoria',
+						'$this->genero',
+						'$this->estado'
+						)";
+
+
+				if (!$this->mysqli->query($sql)) {
+					return 'Error';
+				}
+				else{
+					return 'Campeonato añadido correctamente';
+				}
+
+			}
+			else
+				return 'Campeonato ya existe';
+		}
+    }
+
+  }
 }
 
  ?>
