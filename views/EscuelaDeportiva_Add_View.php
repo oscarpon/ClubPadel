0954eac5621c90712b5371615c23aa3cb8dde38e
oscarpon <oscar.pontebaquero@gmail.com>
2020-01-21 19:45:39 +0100

    Campo pista

--- views/EscuelaDeportiva_Add_View.php
+++ views/EscuelaDeportiva_Add_View.php
@@ -20,9 +20,9 @@ class EscuelaDeportivaAddView
 <div class="">
   <form class="formularioAñadirCamp" action="../controllers/GestionEscuela_Controller.php?action=ADD" method="post" >
       <input type="text" name="nombre" placeholder="Nombre Escuela Deportiva" onblur="comprobarVacio(nombre)">
-      <input type="datetime-local" name="horario" placeholder="Horario inicio" min="<?php echo $fecha;?>">
+      <input type="datetime" name="horario" placeholder="Horario inicio" min="<?php echo $fecha;?>">
       <input type="email" name="entrenador" placeholder="Entrenador email">
-      <input type="text" name="codigoPista" placeholder="codigo Pista" >
+      <input type="text" name="codigoPista" placeholder="codigo Pista" maxlength="6">
       <input type="text" name="periodicidad" placeholder="periodicidad" maxlength="1" >
       <input type="text" name="minInscritos" placeholder="Minimo Inscritos" maxlength="2" >
       <input type="text" name="maxInscritos" placeholder="Maximo Inscritos" maxlength="2" >
