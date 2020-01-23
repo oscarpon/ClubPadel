197cbf6428ac43d86f04069cf61ac8845c4f61ae
oscarpon <oscar.pontebaquero@gmail.com>
2020-01-23 12:10:23 +0100

    requerido titulo de la noticia

--- views/Noticias_Add_View.php
+++ views/Noticias_Add_View.php
@@ -20,7 +20,7 @@
     <input type="hidden" value="0" name="idContenido" class="form-control form-control-lg" placeholder="Titulo"/>
   </div>
         <div>
-          <input type="text" name="titulo" class="form-control form-control-lg" placeholder="Subtitulo"/>
+          <input type="text" name="titulo" class="form-control form-control-lg" placeholder="Subtitulo" required/>
         </div>
         <div class="form-group my-3">
           <textarea rows="4" cols="50" name="descripcion" class="form-control form-control-lg" placeholder="Contenido de la noticia" required/></textarea>
